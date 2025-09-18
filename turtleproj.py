7e2ff874d894ae45d8862898ac871c371c4ab099
Guru-CS <gurusaran.cs@gmail.com>
2025-09-18 10:42:40 -0400

    Update turtleproj.py

--- turtleproj.py
+++ turtleproj.py
@@ -1,24 +1,48 @@
 import turtle
 import random
 
+# Setup
 t = turtle.Turtle()
-t.speed(40)
-t2 = turtle.Turtle()
-t2.speed(20)
-t3 = turtle.Turtle()
-t3.speed(20)
-t4 = turtle.Turtle()
-t4.speed(20)
-t.circle(50,180)
-t2.circle(50,-180)
-turtle.setworldcoordinates(500,500,-500,-500)
-while True:
- degree = random.randint(0,360)
- x = random.randint(-500,500)
- y = random.randint(-500,500)
- t.goto(x,y)
- t2.goto(-x,y)
- t3.goto(-x,-y)
- t4.goto(x,-y)
+t.speed(0)
+t.hideturtle()
+#how big each line is
+dis = 20
 
+# Ask user for number of mirrored rotations
+ref = int(input("Enter number of mirrored rotations: "))
+# Uses a tuple to gain the coordinates and an array for all the mirrored and rotated lines
+positions = [(0, 0)] * (ref * 2)
 
+# Main loop, adds a line on to the previous line
+for _ in range(100):
+ # random direction for all the new lines
+    dire = random.randint(-360, 360)
+
+    for i in range(ref):
+     # what rotates the orignal line
+        angle_offset = 360 / ref * i
+
+        # Original line
+        t.penup()
+        t.goto(positions[i * 2])
+        t.pendown()
+        heading = dire + angle_offset
+        t.setheading(heading)
+        t.forward(dis)
+        positions[i * 2] = (t.xcor(), t.ycor())
+
+        # Mirrored line
+        t.penup()
+        t.goto(positions[i * 2 + 1])
+        t.pendown()
+     #negative direction so its mirrored
+        heading = -dire + angle_offset
+        t.setheading(heading)
+        t.forward(dis)
+     # saves the position for the endpoint of the line created
+        positions[i * 2 + 1] = (t.xcor(), t.ycor())
+
+    # Return to first position
+    t.penup()
+    t.goto(positions[0])
+    t.pendown()
